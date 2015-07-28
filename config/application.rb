bcb6dfe5b393c7964be095d0f3d411c3ab95ba57
Ariel Lopez <ariel.lopezf@gmail.com>
2015-07-29 00:52:48 +0800

    setting environment angularails

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Angularails
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generators do|g|
+    config.generators do |g|
       g.stylesheets false
       g.helper false
       g.javascripts false
