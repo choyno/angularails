67ffac9e39103bd809f11373c8a442bc93f301f9
Ariel Lopez <ariel.lopezf@gmail.com>
2015-07-29 01:04:20 +0800

    adding secret base key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,3 @@
+i
+
+Angularails::Application.config.secret_key_base = 'c46e57238c5883d1158e23eb04e7a08ca5f6addd33c05b7750006421d9eeb7a681af9ea692327aa7fa8906b564a7fd59a6154d8b4278403406ab53365478f2e9'
